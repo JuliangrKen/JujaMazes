8b8078db954c263935570a48281769329dd0117c
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-11 18:11:59 +0300

    Описал интерфейс алгоритмов

---
+++ JujaMazes.Lib/IMazeAlgorithm.cs
@@ -0,0 +1,10 @@
+﻿using JujaMazes.Lib.Models;
+
+namespace JujaMazes.Lib
+{
+    public interface IMazeAlgorithm
+    {
+        string AlgorithmName { get; }
+        Maze GenerateMaze(int weight, int height); 
+    }
+}
