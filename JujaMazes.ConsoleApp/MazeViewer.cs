989c8604b55ac9f86e0b1853ae456f008a7ffcb7
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-11 19:02:41 +0300

    Замутил первую вьюшку

---
+++ JujaMazes.ConsoleApp/MazeViewer.cs
@@ -0,0 +1,33 @@
+﻿using JujaMazes.Lib.Models;
+using JujaMazes.Lib.Utils;
+
+namespace JujaMazes.ConsoleApp
+{
+    internal class MazeViewer
+    {
+        /// Временное решение для дебага. Стоит переписать алгоритм.
+        public string GetMazeAscii(Maze maze)
+        {
+            var str = string.Empty;
+
+            var matrix = maze.GetCellsMatrix();
+
+            for(int i = 0; i < maze.Height; i++)
+            {
+                str += '|';
+
+                for (int j = 0; j < maze.Width; j++)
+                {
+                    var cell = matrix[i, j];
+
+                    str += cell.BottomWall ? '_' : ' ';
+                    str += cell.RightWall ?  '|' : ' ';
+                }
+
+                str += '\n';
+            }
+
+            return str;
+        }
+    }
+}
--- JujaMazes.ConsoleApp/Program.cs
+++ JujaMazes.ConsoleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using JujaMazes.ConsoleApp;
+using JujaMazes.Lib.Models;
+
+// Используется простейший лабиринт для дебага
+var maze = new Maze()
+{
+    Height = 2,
+    Width = 2,
+    Cells = new List<Cell>()
+    {
+        new Cell() { BottomWall = false, RightWall = false },
+        new Cell() { BottomWall = true, RightWall = true },
+        new Cell() { BottomWall = true, RightWall = false },
+        new Cell() { BottomWall = false, RightWall = true },
+    }
+};
+
+Console.WriteLine(new MazeViewer().GetMazeAscii(maze));
