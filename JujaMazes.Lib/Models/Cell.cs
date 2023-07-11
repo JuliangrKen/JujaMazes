508bfb92f43d96e23900b0df35044f5bf2b79f17
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-11 17:57:27 +0300

    Создал модель ячейки

---
+++ JujaMazes.Lib/Models/Cell.cs
@@ -0,0 +1,10 @@
+﻿namespace JujaMazes.Lib.Models
+{
+    public class Cell
+    {
+        public bool TopWall { get; set; }
+        public bool BottomWall { get; set; }
+        public bool RightWall { get; set; }
+        public bool LeftWall { get; set; }
+    }
+}
