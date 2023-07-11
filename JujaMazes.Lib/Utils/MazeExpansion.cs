dbd2dc403e4d38d8aa20760155006ccec0445154
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-11 19:02:30 +0300

    Сделал расширение для удобства.

---
+++ JujaMazes.Lib/Utils/MazeExpansion.cs
@@ -0,0 +1,28 @@
+﻿using JujaMazes.Lib.Models;
+
+namespace JujaMazes.Lib.Utils
+{
+    public static class MazeExpansion
+    {
+        public static Cell[,] GetCellsMatrix(this Maze maze)
+        {
+            if (maze.Cells == null)
+                throw new ArgumentNullException(nameof(maze));
+
+            var cells = new Cell[maze.Height, maze.Width];
+
+            var curCellsIndex = 0;
+
+            for (int i = 0; i < maze.Height; i++)
+            {
+                for (int j = 0; j < maze.Width; j++)
+                {
+                    cells[i, j] = maze.Cells[curCellsIndex];
+                    curCellsIndex++;
+                }
+            }
+
+            return cells;
+        }
+    }
+}
