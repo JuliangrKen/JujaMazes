1a9d372f3413d8c15e8c23588f940236a40dd72c
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-11 18:00:12 +0300

    Создал модель лабиринта

---
+++ JujaMazes.Lib/Models/Maze.cs
@@ -0,0 +1,9 @@
+﻿namespace JujaMazes.Lib.Models
+{
+    public class Maze
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public List<Maze>? Cells { get; set; }
+    }
+}
