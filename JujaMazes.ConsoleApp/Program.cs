1b2488190712ea46885b68d6ec45e265ad09f1bd
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-12 15:23:52 +0300

    Удалил закомментированный код

--- JujaMazes.ConsoleApp/Program.cs
+++ JujaMazes.ConsoleApp/Program.cs
@@ -1,26 +1,6 @@
 ﻿using JujaMazes.ConsoleApp;
 using JujaMazes.Lib;
 using JujaMazes.Lib.Enums;
-using JujaMazes.Lib.Models;
-using JujaMazes.Lib.Utils;
-
-//// Используется простейший лабиринт для дебага
-//var maze = new Maze()
-//{
-//    Height = 2,
-//    Width = 2,
-//    Cells = new List<Cell>()
-//    {
-//        new Cell() { TopWall = true, LeftWall = false },
-//        new Cell() { TopWall = true, LeftWall = false },
-//        new Cell() { TopWall = true, LeftWall = false },
-//        new Cell() { TopWall = false, LeftWall = true },
-//    }
-//};
-
-//maze.Normalize();
-
-//Console.WriteLine(new MazeViewer().GetMazeAscii(maze));
 
 var algFactory = new AlgorithmFactory() { MazeHeight = 5, MazeWidth = 5 };
 var alg = algFactory.GetAlgorithm(Algorithm.Eller);
