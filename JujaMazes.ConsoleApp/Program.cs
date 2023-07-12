63d607b3fa0ddf68762c0eef170e4d547bb7ad19
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-12 14:45:02 +0300

    Теперь тестим сам алгоритм

--- JujaMazes.ConsoleApp/Program.cs
+++ JujaMazes.ConsoleApp/Program.cs
@@ -1,18 +1,28 @@
 ﻿using JujaMazes.ConsoleApp;
+using JujaMazes.Lib;
+using JujaMazes.Lib.Enums;
 using JujaMazes.Lib.Models;
+using JujaMazes.Lib.Utils;
 
-// Используется простейший лабиринт для дебага
-var maze = new Maze()
-{
-    Height = 2,
-    Width = 2,
-    Cells = new List<Cell>()
-    {
-        new Cell() { BottomWall = false, RightWall = false },
-        new Cell() { BottomWall = true, RightWall = true },
-        new Cell() { BottomWall = true, RightWall = false },
-        new Cell() { BottomWall = false, RightWall = true },
-    }
-};
+//// Используется простейший лабиринт для дебага
+//var maze = new Maze()
+//{
+//    Height = 2,
+//    Width = 2,
+//    Cells = new List<Cell>()
+//    {
+//        new Cell() { TopWall = true, LeftWall = false },
+//        new Cell() { TopWall = true, LeftWall = false },
+//        new Cell() { TopWall = true, LeftWall = false },
+//        new Cell() { TopWall = false, LeftWall = true },
+//    }
+//};
 
-Console.WriteLine(new MazeViewer().GetMazeAscii(maze));
+//maze.Normalize();
+
+//Console.WriteLine(new MazeViewer().GetMazeAscii(maze));
+
+var algFactory = new AlgorithmFactory() { MazeHeight = 5, MazeWidth = 5 };
+var alg = algFactory.GetAlgorithm(Algorithm.Eller);
+
+Console.WriteLine(new MazeViewer().GetMazeAscii(alg.GenerateMaze()));
