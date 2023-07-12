25859fde9b3b98abc14daf5223c64131dc2f6957
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-12 20:52:26 +0300

    Улучшил вьювер

--- JujaMazes.ConsoleApp/MazeViewer.cs
+++ JujaMazes.ConsoleApp/MazeViewer.cs
@@ -7,27 +7,53 @@ namespace JujaMazes.ConsoleApp
     {
         public string GetMazeAscii(Maze maze)
         {
+            var matrix = maze.GetCellsMatrix();
+
+            var str = CreateTopWallInFirstLine(maze, matrix);
+
             var numCharsInLine = maze.Width * 2 + 1;
 
-            var str = new string(new byte[numCharsInLine].Select(x => '_').ToArray()) + '\n';
+            for (int i = 0; i < maze.Height - 1; i++)
+            {
+                str += CreateLine(maze, matrix, numCharsInLine, i);
+            }
 
-            var matrix = maze.GetCellsMatrix();
+            str += CreateLine(maze, matrix, numCharsInLine, maze.Height - 1, '_');
+
+            return str;
+        }
+
+        private string CreateTopWallInFirstLine(Maze maze, Cell[,] matrix)
+        {
+            var str = string.Empty;
 
             for (int i = 0; i < maze.Height; i++)
             {
-                var row = new char[numCharsInLine];
-
-                var rIndex = 0;
-                row[rIndex++] = matrix[i, 0].LeftWall ? '|' : ' ';
-                
-                for (int j = 0; j < maze.Width; j++)
-                {
-                    row[rIndex++] = matrix[i, j].BottomWall ? '_' : ' ';
-                    row[rIndex++] = matrix[i, j].RightWall ? '|' : ' ';
-                }
-
-                str += new string(row) + '\n';
+                str += '_';
+                str += matrix[0, i].TopWall ? '_' : ' ';
             }
+
+            str += "_\n";
+
+            return str;
+        }
+
+        private string CreateLine(Maze maze, Cell[,] matrix, int numCharsInLine, int rowIndex, char rWallDefault = ' ')
+        {
+            var str = string.Empty;
+
+            var row = new char[numCharsInLine];
+
+            var rIndex = 0;
+            row[rIndex++] = matrix[rowIndex, 0].LeftWall ? '|' : ' ';
+
+            for (int j = 0; j < maze.Width; j++)
+            {
+                row[rIndex++] = matrix[rowIndex, j].BottomWall ? '_' : ' ';
+                row[rIndex++] = matrix[rowIndex, j].RightWall ? '|' : rWallDefault;
+            }
+
+            str += new string(row) + '\n';
 
             return str;
         }
