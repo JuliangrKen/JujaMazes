584c28582aae46908e5e892ee5a8f3e26c102dfa
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-12 21:08:05 +0300

    Ещё круче улучшил вью

--- JujaMazes.ConsoleApp/MazeViewer.cs
+++ JujaMazes.ConsoleApp/MazeViewer.cs
@@ -5,6 +5,11 @@ namespace JujaMazes.ConsoleApp
 {
     internal class MazeViewer
     {
+        private const char VerticalWall = '|';
+        private const char NoVerticalWall = '.';
+        private const char HorisontalWall = '_';
+        private const char NoHorisontalWall = ' ';
+
         public string GetMazeAscii(Maze maze)
         {
             var matrix = maze.GetCellsMatrix();
@@ -13,32 +18,30 @@ namespace JujaMazes.ConsoleApp
 
             var numCharsInLine = maze.Width * 2 + 1;
 
-            for (int i = 0; i < maze.Height - 1; i++)
+            for (int i = 0; i < maze.Height; i++)
             {
                 str += CreateLine(maze, matrix, numCharsInLine, i);
             }
 
-            str += CreateLine(maze, matrix, numCharsInLine, maze.Height - 1, '_');
-
             return str;
         }
 
-        private string CreateTopWallInFirstLine(Maze maze, Cell[,] matrix)
+        private static string CreateTopWallInFirstLine(Maze maze, Cell[,] matrix)
         {
             var str = string.Empty;
 
             for (int i = 0; i < maze.Height; i++)
             {
-                str += '_';
-                str += matrix[0, i].TopWall ? '_' : ' ';
+                str += NoVerticalWall;
+                str += GetHorisontalWallChar(matrix[0, i].TopWall);
             }
 
-            str += "_\n";
+            str += NoVerticalWall + "\n";
 
             return str;
         }
 
-        private string CreateLine(Maze maze, Cell[,] matrix, int numCharsInLine, int rowIndex, char rWallDefault = ' ')
+        private static string CreateLine(Maze maze, Cell[,] matrix, int numCharsInLine, int rowIndex)
         {
             var str = string.Empty;
 
@@ -49,13 +52,19 @@ namespace JujaMazes.ConsoleApp
 
             for (int j = 0; j < maze.Width; j++)
             {
-                row[rIndex++] = matrix[rowIndex, j].BottomWall ? '_' : ' ';
-                row[rIndex++] = matrix[rowIndex, j].RightWall ? '|' : rWallDefault;
+                row[rIndex++] = GetHorisontalWallChar(matrix[rowIndex, j].BottomWall);
+                row[rIndex++] = GetVerticalWallChar(matrix[rowIndex, j].RightWall);
             }
 
             str += new string(row) + '\n';
 
             return str;
         }
+
+        private static char GetHorisontalWallChar(bool exist)
+            => exist ? HorisontalWall : NoHorisontalWall;
+
+        private static char GetVerticalWallChar(bool exist)
+            => exist ? VerticalWall : NoVerticalWall;
     }
 }
