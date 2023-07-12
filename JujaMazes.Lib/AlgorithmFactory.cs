c3c99fe5f46b139019782937f701b50feaf3b599
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-12 14:44:42 +0300

    Реализовал удобную факторию

---
+++ JujaMazes.Lib/AlgorithmFactory.cs
@@ -0,0 +1,18 @@
+﻿using JujaMazes.Lib.Algorithms;
+using JujaMazes.Lib.Enums;
+
+namespace JujaMazes.Lib
+{
+    public class AlgorithmFactory : IAlgorithmFactory
+    {
+        public int MazeWidth { get; set; }
+        public int MazeHeight { get; set; }
+
+        public IMazeAlgorithm GetAlgorithm(Algorithm algorithm)
+            => algorithm switch
+            {
+                Algorithm.Eller => new EllerAlgorithm() { MazeWidth = MazeWidth, MazeHeight = MazeHeight },
+                _ => throw new NotImplementedException(),
+            };
+    }
+}
