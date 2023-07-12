1fbe67ab7681d4a09a89fbf8fa30ebe4140d00da
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-12 19:36:31 +0300

    Исправил глупый баг во вьюшке

--- JujaMazes.ConsoleApp/MazeViewer.cs
+++ JujaMazes.ConsoleApp/MazeViewer.cs
@@ -7,7 +7,7 @@ namespace JujaMazes.ConsoleApp
     {
         public string GetMazeAscii(Maze maze)
         {
-            var numCharsInLine = (maze.Width - 1) * 2 + 1;
+            var numCharsInLine = maze.Width * 2 + 1;
 
             var str = new string(new byte[numCharsInLine].Select(x => '_').ToArray()) + '\n';
 
@@ -20,7 +20,7 @@ namespace JujaMazes.ConsoleApp
                 var rIndex = 0;
                 row[rIndex++] = matrix[i, 0].LeftWall ? '|' : ' ';
                 
-                for (int j = 1; j < maze.Width; j++)
+                for (int j = 0; j < maze.Width; j++)
                 {
                     row[rIndex++] = matrix[i, j].BottomWall ? '_' : ' ';
                     row[rIndex++] = matrix[i, j].RightWall ? '|' : ' ';
