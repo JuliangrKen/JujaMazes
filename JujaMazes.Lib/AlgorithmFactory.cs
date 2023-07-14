7865cb15c79944a2fe20ed5abf24e65a290a5abc
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-14 10:19:25 +0300

    Добавил дефолтные значения для высоты и ширины лабиринта

--- JujaMazes.Lib/AlgorithmFactory.cs
+++ JujaMazes.Lib/AlgorithmFactory.cs
@@ -5,8 +5,8 @@ namespace JujaMazes.Lib
 {
     public class AlgorithmFactory : IAlgorithmFactory
     {
-        public int MazeWidth { get; set; }
-        public int MazeHeight { get; set; }
+        public int MazeWidth { get; set; } = 5;
+        public int MazeHeight { get; set; } = 5;
         public IDecisionMaker DecisionMaker { get; set; } = new DefaultDecisionMaker();
 
         public IMazeAlgorithm GetAlgorithm(Algorithm algorithm)
