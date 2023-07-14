335f07d2561776690e32ded6f2cc957c7d11939c
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-14 10:31:26 +0300

    Добавил прикольные конструкторы

--- JujaMazes.Lib/AlgorithmFactory.cs
+++ JujaMazes.Lib/AlgorithmFactory.cs
@@ -5,9 +5,34 @@ namespace JujaMazes.Lib
 {
     public class AlgorithmFactory : IAlgorithmFactory
     {
-        public int MazeWidth { get; set; } = 5;
-        public int MazeHeight { get; set; } = 5;
-        public IDecisionMaker DecisionMaker { get; set; } = new DefaultDecisionMaker();
+        public int MazeWidth { get; set; }
+        public int MazeHeight { get; set; }
+        public IDecisionMaker DecisionMaker { get; set; }
+
+        #region constructors
+        public AlgorithmFactory(int mazeWidth, int mazeHeight) : this(mazeWidth, mazeHeight, new DefaultDecisionMaker())
+        {
+        }
+
+        public AlgorithmFactory(int size, IDecisionMaker decisionMaker) : this(size, size, decisionMaker)
+        {
+        }
+
+        public AlgorithmFactory(int size) : this(size, size, new DefaultDecisionMaker())
+        {
+        }
+
+        public AlgorithmFactory(IDecisionMaker decisionMaker) : this(5, 5, decisionMaker)
+        {
+        }
+        #endregion
+
+        public AlgorithmFactory(int mazeWidth, int mazeHeight, IDecisionMaker decisionMaker)
+        {
+            MazeWidth = mazeWidth;
+            MazeHeight = mazeHeight;
+            DecisionMaker = decisionMaker;
+        }
 
         public IMazeAlgorithm GetAlgorithm(Algorithm algorithm)
             => algorithm switch
